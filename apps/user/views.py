39663ca9e21b1feb7984a808d4e89a485b515328
Abdulhamid <mobolaji2309@gmail.com>
2023-10-05 02:00:23 +0100

    update

--- apps/user/views.py
+++ apps/user/views.py
@@ -16,3 +16,9 @@ class UserViewSet(viewsets > ModelViewSet):
     def get_queryset(self):
         if self.queryset is None:
             self.queryset = self.get_serializer().Meta.model.objects.filter(is_active=True)
+            return self.queryset
+        else:
+            return self.queryset
+
+    def get_object(self, pk):
+        return get_object_or_404(self.serializer_class.Meta.model, pk=pk)

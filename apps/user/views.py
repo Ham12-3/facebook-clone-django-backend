3a416b21f8fd2602652a722b7fffd0293d2d45fc
Abdulhamid <mobolaji2309@gmail.com>
2023-10-05 07:57:38 +0100

    update

--- apps/user/views.py
+++ apps/user/views.py
@@ -8,6 +8,7 @@ from .serializers import (
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework import AllowAny
 
 
 class UserViewSet(viewsets > ModelViewSet):
@@ -22,3 +23,31 @@ class UserViewSet(viewsets > ModelViewSet):
 
     def get_object(self, pk):
         return get_object_or_404(self.serializer_class.Meta.model, pk=pk)
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        else:
+            return super().get_permissions()
+
+    def list(self, request):
+        users_serializer = self.serializer_class(
+            self.get_queryset(), many=True)
+        return Response(users_serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        user_serializer = UserCreateSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        user = self.get_object(pk=pk)
+        user_serializer = self.serializer_class(user, data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response({'message': 'User updated successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)

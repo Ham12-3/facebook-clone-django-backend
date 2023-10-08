9d9a45e2c8613ca1e0c52c8f9676c9c5e91e3406
Abdulhamid <mobolaji2309@gmail.com>
2023-10-08 23:00:41 +0100

    update

--- apps/post/views.py
+++ apps/post/views.py
@@ -10,6 +10,10 @@ from django.shortcuts import get_object_or_404
 from apps.user.models import CustomPagination
 
 
+def is_owner(request, instance):
+    return request.user == instance.author or request.user.is_staff  # boolean value
+
+
 class PostViewSet(viewsets.ModeViewSet):
     serializer_class = PostSerializer
 
@@ -43,3 +47,17 @@ class PostViewSet(viewsets.ModeViewSet):
         posts = self.get_object(pk=pk)
         post_serializer = self.serializer_class(posts)
         return Response(post_serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        post = self.get_object(pk=pk)
+        if not is_owner(request, post):
+            return Response({'message': 'You are not authorized to perform this action'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            if 'image' not in request.data or request.data['image'] == '':
+                data = request.data.copy()
+                current_image = post.image
+                data['image'] = current_image
+
+                post_serializer = PostSerializer(post, data=data)
+                if post_serializer.is_valid():
+                    return Response({'message': 'Post updated successfully'}, status=status.HTTP_200_OK)

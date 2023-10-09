46fe99fcf9491bdfb63668c0522a8c28706b0629
Abdulhamid <mobolaji2309@gmail.com>
2023-10-09 11:26:46 +0100

    update

---
+++ apps/comment/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    author_image = serializers.ReadOnlyField(source='author.image.url')
+    post = serializers.ReadOnlyField(source='post')
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'

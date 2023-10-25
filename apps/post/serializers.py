0a0e50957f75e04382af625b406dcf63d7785b80
Ham12-3 <mobolaji2309@gmail.com>
2023-10-25 08:02:32 +0100

    updat

--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -9,6 +9,7 @@ class PostSerializer(serializers.ModelSerializer):
     author_id = serializers.ReadOnlyField(source='author.id')
     likes = serializers.SerializerMethodField()
     comment = CommentSerializer(many=True, read_only=True)
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
@@ -16,6 +17,11 @@ class PostSerializer(serializers.ModelSerializer):
 
     def get_likes(self, obj):
         return [user.username for user in obj.likes.all()]
+
+    # Env
+
+    def get_image(self, obj):
+        return obj.image.url.replace('http://localhost:8000', '')
 
 
 class PostCreateSerializer(serializers.ModelSerializer):
